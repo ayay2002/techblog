c14e3fe488eadcc8e568afc534a10c10d6bb63e8
Alexander <alexandercryptock@icloud.com>
2024-02-14 19:19:14 -0500

    added json file

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require("./User");
 const Blog = require("./Blog");
-const Comment = require("./Comment");
+const Comment = require("./Comments");
 
 User.hasMany(Blog, {
   foreignKey: "user_id",
