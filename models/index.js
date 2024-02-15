13ab8f5f775efcf00208ba84bd097d1bd7e8d8c6
Alexander <alexandercryptock@icloud.com>
2024-02-14 21:12:25 -0500

    bug fix

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require("./User");
-const Blog = require("./Blog");
-const Comment = require("./Comments");
+const User = require("./user");
+const Blog = require("./blog");
+const Comment = require("./comments");
 
 User.hasMany(Blog, {
   foreignKey: "user_id",
